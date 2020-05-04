95eafdecf0870170a71e015a5c16f099fc7b7935
leander-dsouza <leanderdsouza1234@gmail.com>
2020-05-05 00:01:05 +0530

    .

---
+++ For Your Eyes Only/decoder.py
@@ -0,0 +1,13 @@
+import base64
+
+MESSAGE = '''
+F0ISGwcGFxcASFVAQRZVQVENEUZCREIRCx8DEBsGRFcUFFZFRgsXERcBHgoRXU0RFVZSCgoTGhdC Ul5TSBwUAkNXV10OCQRJSEVVBRAHHB8XVF9WWhhCQVREQgcKHwAWEQRVFR8USxcADAYMBhdUT09a RkJTVVFLSUFJAgodQ1NVVV0WWFwSExE=
+''' #<encrypted>''</encrypted>
+
+KEY = 'leanderdsouza1234' #your username
+
+result = []
+for i, c in enumerate(base64.b64decode(MESSAGE)):
+    result.append(chr(c ^ ord(KEY[i % len(KEY)])))
+
+print(''.join(result))
---
+++ Level 4/running-with-bunnies/Solution.java
@@ -0,0 +1,5 @@
+public​ ​class​ ​Solution​ ​{
+​ ​​ ​​ ​​ ​public​ ​static​ ​int[]​ ​solution(int[][]​ ​times,​ ​int​ ​times_limit)​ ​{
+​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​//​ ​Your​ ​code​ ​here
+​ ​​ ​​ ​​ ​}
+}
